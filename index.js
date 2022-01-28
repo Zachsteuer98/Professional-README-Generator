0300f2a52d16e7194229bc5b26b39f1e36205eda
Zachsteuer98 <Zach.Steuer98@gmail.com>
2022-01-28 14:48:04 -0500

    saving work up to this point

--- index.js
+++ index.js
@@ -2,15 +2,47 @@
 const inquirer = require('inquirer');
 // const fs = require('fs');
 // const generatePage = require('../Professional-README-Generator/utils/page-template');
-inquirer
-  .prompt([
+const promptUser = () => {
+ return inquirer.prompt([
       {
           type: 'input',
           name: 'repoName',
           message: 'What is the name of your website/project?'
+      },
+      {
+          type: 'input',
+          name: 'description',
+          message: 'What is a brief description explaining the what, why, and how of your project.'
+      },
+      {
+          type: 'input',
+          name: 'installation',
+          message: 'What are the steps required to install your project? Are there none?'
+      },
+      {
+          type: 'checkbox',
+          name: 'languages',
+          message: 'What programming languages/libraries were used to create the application?',
+          choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'JQuery', 'Bootstrap', 'Node', 'Bulma']
+      },
+      {
+          type: 'input',
+          name: 'usage',
+          message: 'Provide instructions and examples for use.' 
+      },
+      {
+          type: 'input',
+          name: 'features',
+          message: 'What features does your application have?'
+      },
+      {
+          type: 'input',
+          name: 'contributors',
+          message: 'Who are the contributors for this application?'
       }
   ])
-  .then(answers => console.log(answers));
+};
+  promptUser().then(answers => console.log(answers));
 
 
 
